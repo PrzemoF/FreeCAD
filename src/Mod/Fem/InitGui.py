807c0e1242dade15503c512ce8ae538f05567b44
Przemo Firszt <przemo@firszt.eu>
2015-04-07 15:40:43 +0100

    FEM: Fix replace hardcoded icon commit
    Test to fix 3f11a9a513e59bafd0d4d40cb1093af05970e1fe

--- src/Mod/Fem/InitGui.py
+++ src/Mod/Fem/InitGui.py
@@ -34,13 +34,15 @@
 class FemWorkbench ( Workbench ):
 	"Fem workbench object"
         def __init__(self):
-            self.__class__.Icon = "./Mod/Fem/Gui/Resources/icons/preferences-fem.svg"
-
+            self.__class__.Icon = FreeCAD.getResourceDir()+"Mod/Fem/Gui/Resources/icons/preferences-fem.svg"
+            self.__class__.MenuText = "FEM"
+            self.__class__.ToolTip = "FEM workbench"
 
 	def Initialize(self):
 		# load the module
 		import Fem
 		import FemGui
+
 	def GetClassName(self):
 		return "FemGui::Workbench"
 
