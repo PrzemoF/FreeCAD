d27951f71dc924073d49f34ace50ab0da1f227b9
Przemo Firszt <przemo@firszt.eu>
2015-10-10 14:28:42 +0100

    FEM: FemCommand class - first commit - wip
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

---
+++ src/Mod/Fem/FemCommands.py
@@ -0,0 +1,53 @@
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2015 - FreeCAD Developers                               *
+#*   Author (c) 2015 - Przemo Fiszt < przemo@firszt.eu>                    *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+
+__title__ = "Fem Commands"
+__author__ = "Przemo Firszt"
+__url__ = "http://www.freecadweb.org"
+
+import FreeCAD
+
+if FreeCAD.GuiUp:
+    import FreeCADGui
+    import FemGui
+    from PySide import QtCore
+
+
+class FemCommands(object):
+        def __init__(self):
+            self.resources = {'Pixmap': 'fem-frequency-analysis',
+                              'MenuText': QtCore.QT_TRANSLATE_NOOP("Fem_Command", "Default Fem Command MenuText"),
+                              'Accel': "",
+                              'ToolTip': QtCore.QT_TRANSLATE_NOOP("Fem_Command", "Default Fem Command ToolTip")}
+            #FIXME add option description
+            self.is_active = None
+
+        def GetResources(self):
+            return self.resources
+
+        def IsActive(self):
+            if not self.is_active:
+                active = False
+            elif self.is_active == 'default':
+                active = FreeCADGui.ActiveDocument is not None and FemGui.getActiveAnalysis() is not None
+            return active
--- src/Mod/Fem/_CommandFrequencyAnalysis.py
+++ src/Mod/Fem/_CommandFrequencyAnalysis.py
@@ -1,4 +1,31 @@
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2013-2015 - Juergen Riegel <FreeCAD@juergen-riegel.net> *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+
+__title__ = "Command Frequency Analysis"
+__author__ = "Juergen Riegel"
+__url__ = "http://www.freecadweb.org"
+
 import FreeCAD
+import FemCommands
 from FemTools import FemTools
 
 if FreeCAD.GuiUp:
@@ -7,12 +34,14 @@ if FreeCAD.GuiUp:
     from PySide import QtCore, QtGui
 
 
-class _CommandFrequencyAnalysis:
-    def GetResources(self):
-        return {'Pixmap': 'fem-frequency-analysis',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Fem_Frequency_Analysis", "Run frequency analysis with CalculiX ccx"),
-                'Accel': "R, F",
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Fem_Frequency_Analysis", "Write .inp file and run frequency analysis with CalculiX ccx")}
+class _CommandFrequencyAnalysis(FemCommands):
+    def __init__(self):
+        FemCommands.__init__(self)
+        self.resources = {'Pixmap': 'fem-frequency-analysis',
+                          'MenuText': QtCore.QT_TRANSLATE_NOOP("Fem_Frequency_Analysis", "Run frequency analysis with CalculiX ccx"),
+                          'Accel': "R, F",
+                          'ToolTip': QtCore.QT_TRANSLATE_NOOP("Fem_Frequency_Analysis", "Write .inp file and run frequency analysis with CalculiX ccx")}
+        self.is_active = 'default'
 
     def Activated(self):
         def load_results(ret_code):
@@ -31,9 +60,15 @@ class _CommandFrequencyAnalysis:
         self.fea.finished.connect(load_results)
         QtCore.QThreadPool.globalInstance().start(self.fea)
 
-    def IsActive(self):
-        return FreeCADGui.ActiveDocument is not None and FemGui.getActiveAnalysis() is not None
+    def GetResources(self):
+        return self.resources
 
+    def IsActive(self):
+        if not self.is_active:
+            active = False
+        elif self.is_active == 'default':
+            active = FreeCADGui.ActiveDocument is not None and FemGui.getActiveAnalysis() is not None
+        return active
 
 if FreeCAD.GuiUp:
     FreeCADGui.addCommand('Fem_Frequency_Analysis', _CommandFrequencyAnalysis())
