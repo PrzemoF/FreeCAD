71d90bbf19e0ec1ca11015794aef586ebaa4e7d6
Przemo Firszt <przemo@firszt.eu>
2022-09-19 21:32:07 +0100

    Revert "Try different path for zipios files"
    This reverts commit e1689cf6b7d7818082bb4d6a6b1e2cf41487e4f3.

--- src/Base/ZipHeader.h
+++ src/Base/ZipHeader.h
@@ -25,8 +25,8 @@
 #define ZIPIOS_ZIP_HEADER_H
 
 #include <FCGlobal.h>
-#include <backbuffer.h>
-#include <fcoll.h>
+#include <zipios++/backbuffer.h>
+#include <zipios++/fcoll.h>
 
 namespace zipios
 {
